1edf86e9558c43b09e090b2332016ab7f21201a2
ManalTalli <manal5talli@gmail.com>
2026-01-17 23:20:59 +0200

    hero

--- src/assets/hero/Hero.jsx
+++ src/assets/hero/Hero.jsx
@@ -1,10 +1,15 @@
 import style from './Hero.module.css'
+import imgboy from '../img/hero/boy.svg'
 import React from 'react'
 
 export default function Hero() {
   return (
     <div>
-      
+      <div className={`${style.bg1ABC9C} d-flex flex-column justify-content-center align-items-center`}>
+      <img src={imgboy} className={`${style.imgb} pt-3`} alt="boy" />
+      <h1 className={`${style.title} pt-5`}>Start Bootstrap</h1>
+      <p className={`${style.par} pt-5`}>Graphic Artist - Web Designer - Illustrator</p>
+      </div>
     </div>
   )
 }
