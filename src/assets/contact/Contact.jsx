5e4155bd24250f21de37fdcd4a5afb15979e9e8d
ManalTalli <manal5talli@gmail.com>
2026-01-18 01:14:08 +0200

    fppter

--- src/assets/contact/Contact.jsx
+++ src/assets/contact/Contact.jsx
@@ -3,8 +3,61 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>
-      
-    </div>
-  )
+    <div className='container d-flex flex-column justify-content-center align-items-center pb-5'>
+
+      <h2 className={`${style.title}`}>Contact Me</h2>
+
+      <div className="container ">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <form  className="custom-form">
+
+              <div className="mb-4">
+                <input
+                  type="text"
+                  name="fullName"
+                  className="form-control custom-input"
+                  placeholder="Full name"
+                  />
+              </div>
+
+              <div className="mb-4">
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control custom-input"
+                  placeholder="Email address"
+                />
+              </div>
+
+              <div className="mb-4">
+                <input
+                  type="tel"
+                  name="phone"
+                  className="form-control custom-input"
+                  placeholder="Phone number"
+                />
+              </div>
+
+              <div className="mb-4 position-relative">
+                <label className="text-muted mb-2 d-block">Message</label>
+                <textarea
+                  name="message"
+                  className="form-control custom-input custom-textarea"
+                  rows="3"
+                  ></textarea>
+                <div className="textarea-resize-icon"></div>
+              </div>
+
+              <button type="submit" className="btn btn-send shadow-sm">
+                Send
+              </button>
+
+            </form>
+          </div>
+        </div>
+      </div>
+            </div>
+
+      )
 }
--- src/assets/footer/Footer.jsx
+++ src/assets/footer/Footer.jsx
@@ -3,8 +3,23 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className={`${style.bg2C3E50}`}>
+      <div className={`container d-flex justify-content-between`}>
+        <div>
+          <h3 className={`${style.title}`}>Location</h3>
+          <p>2215 John Daniel Drive<br/>
+Clark, MO 65243</p>
+        </div>
+
+        <div>
+          <h3 className={`${style.title}`}>Around the Web</h3>
+
+        </div>
+        <div>
+          <h3 className={`${style.title}`}>About Freelancer</h3>
+          <p>Freelance is a free to use, MIT licensed<br/> Bootstrap theme created by Start Bootstrap .</p>
+      </div>
+      </div>
     </div>
   )
 }
